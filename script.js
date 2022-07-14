5c819f69314b6502b6acb32c32a90731c07a5aa5
Érica Joice dos Santos <ejoice19@gmail.com>
2022-07-14 11:59:09 -0300

    hora formatada

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 //*Puxar a data e hora local******/
 const data = new Date();
-
 let daySpan = document.querySelector(".day");
 let dayNumber = document.querySelector(".dayNumber");
 let monthSpan = document.querySelector(".month");
 let year = document.querySelector(".year");
 
-let hour= document.querySelector(".hour");
-let minutes= document.querySelector(".minutes");
-let seconds= document.querySelector(".seconds");
+let hourSpan= document.querySelector(".hour");
+let minutesSpan= document.querySelector(".minutes");
+let secondsSpan= document.querySelector(".seconds");
 
 /**Converter numeros para String */
+function formatData(){
 const months = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
 
 const days =["Domingo", "Segunda-feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira","Sexta-Feira", "Sábado"]
@@ -23,21 +23,33 @@ daySpan.innerHTML = day;
 dayNumber.innerHTML = data.getDate();
 monthSpan.innerHTML = month;
 year.innerHTML = data.getFullYear();
-
-hour.innerHTML = data.getHours();
-minutes.innerHTML = data.getMinutes();
-seconds.innerHTML = data.getSeconds();
-
-
-
-
-
-
-
-
+}
 
 //*Formatar a data;*******/
+function formatHours(){
+
+let hour = data.getHours();
+let minutes = data.getMinutes();
+let seconds = data.getSeconds(); 
+
+if(hour<10)  hour = "0"+hour;
+if(minutes<10) minutes = "0" +minutes;
+if(seconds<10) seconds = "0" +minutes;
+
+hourSpan.innerHTML = hour
+minutesSpan.innerHTML = minutes
+secondsSpan.innerHTML = seconds
+}
+
 
 //*Data continuar a rodar automaticamente*****/
+function time(hour, minutes, seconds){
+   let hours = hour+":" +minutes+":" +seconds;
+
+}
+
 
 //*Trocar o fundo assimm quem a hora mudar */
+
+formatData();
+formatHours();
