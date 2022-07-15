d599c78ec772aaa53be27d5f412fcf6941fd0e46
Érica Joice dos Santos <ejoice19@gmail.com>
2022-07-15 13:56:45 -0300

    Codigo mais limpo, sem atualizar a hora automaicamente

--- script.js
+++ script.js
@@ -1,55 +1,48 @@
 //*Puxar a data e hora local******/
 const data = new Date();
-let daySpan = document.querySelector(".day");
-let dayNumber = document.querySelector(".dayNumber");
-let monthSpan = document.querySelector(".month");
-let year = document.querySelector(".year");
 
-let hourSpan= document.querySelector(".hour");
-let minutesSpan= document.querySelector(".minutes");
-let secondsSpan= document.querySelector(".seconds");
-
-/**Converter numeros para String */
+//**Converter numeros para String */
+//*Formatar a data;*******/
 function formatData(){
 const months = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
 
 const days =["Domingo", "Segunda-feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira","Sexta-Feira", "Sábado"]
 
 let month = months[data.getMonth()];
-
 let day = days[data.getDay()];
+let dayNumber= data.getDate();
+let year = data.getFullYear();
 
-daySpan.innerHTML = day;
-dayNumber.innerHTML = data.getDate();
-monthSpan.innerHTML = month;
-year.innerHTML = data.getFullYear();
+let date = document.querySelector(".date");
+
+date.innerHTML = `${day}, ${dayNumber} de ${month} de ${year} `
 }
 
-//*Formatar a data;*******/
+
+
+setInterval(formatHours, 1000);
+
 function formatHours(){
 
 let hour = data.getHours();
 let minutes = data.getMinutes();
 let seconds = data.getSeconds(); 
 
+let getTime= document.querySelector(".time");
+
 if(hour<10)  hour = "0"+hour;
 if(minutes<10) minutes = "0" +minutes;
-if(seconds<10) seconds = "0" +minutes;
+if(seconds<10) seconds = "0" +seconds;
 
-hourSpan.innerHTML = hour
-minutesSpan.innerHTML = minutes
-secondsSpan.innerHTML = seconds
-}
+getTime.innerHTML = `${hour}:${minutes}:${seconds}`
+}; 
 
 
 //*Data continuar a rodar automaticamente*****/
-function time(hour, minutes, seconds){
-   let hours = hour+":" +minutes+":" +seconds;
-
-}
 
 
 //*Trocar o fundo assimm quem a hora mudar */
 
 formatData();
-formatHours();
+
+
