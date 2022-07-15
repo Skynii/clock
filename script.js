6730e6a3deca762d76888d8c5c491eefecb788e6
Érica Joice dos Santos <ejoice19@gmail.com>
2022-07-15 14:05:29 -0300

    short variaveis

--- script.js
+++ script.js
@@ -13,14 +13,8 @@ let day = days[data.getDay()];
 let dayNumber= data.getDate();
 let year = data.getFullYear();
 
-let date = document.querySelector(".date");
-
-date.innerHTML = `${day}, ${dayNumber} de ${month} de ${year} `
+document.querySelector(".date").innerHTML = `${day}, ${dayNumber} de ${month} de ${year} `
 }
-
-
-
-setInterval(formatHours, 1000);
 
 function formatHours(){
 
@@ -28,21 +22,22 @@ let hour = data.getHours();
 let minutes = data.getMinutes();
 let seconds = data.getSeconds(); 
 
-let getTime= document.querySelector(".time");
 
 if(hour<10)  hour = "0"+hour;
 if(minutes<10) minutes = "0" +minutes;
 if(seconds<10) seconds = "0" +seconds;
 
-getTime.innerHTML = `${hour}:${minutes}:${seconds}`
+document.querySelector(".time").innerHTML = `${hour}:${minutes}:${seconds}`
+
 }; 
+
+setInterval(formatHours, 1000);
+formatData();
 
 
 //*Data continuar a rodar automaticamente*****/
 
 
 //*Trocar o fundo assimm quem a hora mudar */
-
-formatData();
 
 
